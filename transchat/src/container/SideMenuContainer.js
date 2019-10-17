7d97ae4201c81c78fa07113c2bd5ad27aaa34387
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-10-17 11:56:06 +0100

    minor initial frontend stuff

--- transchat/src/container/SideMenuContainer.js
+++ transchat/src/container/SideMenuContainer.js
@@ -6,7 +6,7 @@ class SideMenuContainer extends React.Component {
 	render() {
 		return (
 			<div className="side-menu-container">
-				SideMenuContainer
+				{/* SideMenuContainer */}
 				<SearchBar />
 				<ListDisplay />
 			</div>
